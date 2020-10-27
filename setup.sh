b4a607afdcf13183283bcbc0793ce9513fe21b5c
Sam Meech-Ward <sam@meech-ward.com>
2020-10-27 12:09:35 -0700

    🤗

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Node
+
+wget -O - https://raw.githubusercontent.com/meech-ward/NodeJs-Raspberry-Pi/master/Install-Node.sh | sudo bash;
+sudo node-install -v 8;
+
+## Pigpio
+
+sudo apt-get install pigpio -y
+npm install pigpio
+
+## bleno
+
+sudo apt-get install bluetooth bluez libbluetooth-dev libudev-dev -y
+npm install bleno
